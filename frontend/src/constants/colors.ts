41605b6f4eb1f16a90a37b944f5672164672778a
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-10-10 14:24:51 +0800

    add base colors

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  // Primary
+  blue500: '#4299E1',
+  blue600: '#3182CE',
+  
+  // Background
+  darkBlue: '#1A2942',
+  
+  // Grayscale
+  white: '#FFFFFF',
+  gray50: '#F7FAFC',
+  gray200: '#E2E8F0',
+  gray300: '#CBD5E0',
+  gray400: '#A0AEC0',
+  gray600: '#718096',
+  gray700: '#4A5568',
+  gray900: '#1A202C',
+};
