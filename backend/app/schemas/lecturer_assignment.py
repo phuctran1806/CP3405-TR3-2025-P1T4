cdab37804c5198cc29406f977183846c6db0071f
FSXplay <skinoftheduck@gmail.com>
2025-11-01 14:13:29 +0800

    Remove redundant schemas and endpoints for lecturer

--- backend/app/schemas/lecturer_assignment.py
+++ backend/app/schemas/lecturer_assignment.py
@@ -19,25 +19,14 @@ class LecturerAssignmentBase(BaseModel):
         orm_mode = True
 
 
-class LecturerAssignmentCreate(BaseModel):
+class LecturerAssignmentRequest(LecturerAssignmentBase):
     """Admin: assign a room to a lecturer."""
     id: str = Field(..., description="ID of the lecturer location")
     subject: str
-    location_id: str
     start_time: datetime
     end_time: datetime
     location_id: str
     user_id: str
-
-
-class LecturerAssignmentUpdate(BaseModel):
-    """Admin: update lecture room details or reassign lecturer."""
-    id: str
-    name: Optional[str]
-    subject: Optional[str]
-    start_time: Optional[datetime]
-    end_time: Optional[datetime]
-    lecturer_email: Optional[str]
 
 
 class LecturerAssignmentResponse(LecturerAssignmentBase):
